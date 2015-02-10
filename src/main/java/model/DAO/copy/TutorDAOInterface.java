7a6d4c13d4f3e1a719ef21e3ad83d8db826614a6
AitorPrat <AitorPrat@gmail.com>
2015-02-10 13:42:09 +0100

    Clase TutorDAO e interface TutorDAO

---
+++ src/main/java/model/DAO/copy/TutorDAOInterface.java
@@ -0,0 +1,30 @@
+package model.DAO.copy;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import model.VO.TutorVO;
+
+public interface TutorDAOInterface {
+
+	// Devuelve una lista con todos los tutores
+	public abstract ArrayList<TutorVO> getlistaTutor();
+
+	// recibe 1 idtutor y devuelve ese tutor
+	public abstract TutorVO getTutor(int id);
+
+	// borra 1 tutor con el id recibido
+	public abstract void deleteTutor(int id);
+
+	// modifica un tutor con el id recibido
+	public abstract int updateTutor(int idTutor, String nombre,
+			String apellido1, String apellido2, String DNI, Date fechaNac,
+			String parentesco, String telefono, String email);
+
+    public void insertarTutor(int idAlumno, String nombre,
+			String apellido1, String apellido2, String DNI, Date fechaNac,
+			String parentesco, String telefono, String email);
+	
+}
