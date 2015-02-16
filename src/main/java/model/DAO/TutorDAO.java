25268a8d87cf3395927a003274492ee6fdf920cc
amartinezbenedi <andrea.martinez.benedi@gmail.com>
2015-02-16 09:55:44 +0100

    cambios en TutorDAO (metido insertarTutor)

--- src/main/java/model/DAO/TutorDAO.java
+++ src/main/java/model/DAO/TutorDAO.java
@@ -56,10 +56,10 @@ public class TutorDAO implements TutorDAOInterface {
 			// conexion.prepareStatement("SELECT * FROM EQ4_BICI");
 			ResultSet rs = st.executeQuery();
 			while (rs.next()) {
-				listaTutores.add(new TutorVO(rs.getInt(1), rs.getInt(2), rs
-						.getString(3), rs.getString(4), rs.getString(5), rs
-						.getString(6), rs.getDate(7), rs.getString(8), rs
-						.getString(9), rs.getString(10)));
+				listaTutores.add(new TutorVO(rs.getInt(1), rs
+						.getString(2), rs.getString(3), rs.getString(4), rs
+						.getString(5), rs.getDate(6), rs.getString(7), rs
+						.getString(8), rs.getString(9)));
 			}
 
 		} catch (Exception e) {
@@ -87,10 +87,10 @@ public class TutorDAO implements TutorDAOInterface {
 			st.setInt(1, id);
 			ResultSet rs = st.executeQuery();
 			if (rs.next()) {
-				tutor = new TutorVO(rs.getInt(1), rs.getInt(2), rs.getString(3),
-						rs.getString(4), rs.getString(5), rs.getString(6),
-						rs.getDate(7), rs.getString(8), rs.getString(9),
-						rs.getString(10));
+				tutor = new TutorVO(rs.getInt(1), rs
+						.getString(2), rs.getString(3), rs.getString(4), rs
+						.getString(5), rs.getDate(6), rs.getString(7), rs
+						.getString(8), rs.getString(9));
 			}				
 		} catch (Exception e) {
 
@@ -150,6 +150,8 @@ public class TutorDAO implements TutorDAOInterface {
 	public boolean insertarTutor(int idAlumno, String nombre, String apellido1,
 			String apellido2, String DNI, Date fechaNac, String parentesco,
 			String telefono, String email) {
+		
+		
 		try{
 			if (ds == null)
 				throw new SQLException("Can't get data source");
@@ -158,27 +160,52 @@ public class TutorDAO implements TutorDAOInterface {
 	
 			if (con == null)
 				throw new SQLException("Can't get database connection");
-	
-			PreparedStatement st = con
-					.prepareStatement("INSERT INTO tutor (idAlumno,nombre,apellido1,apellido2,DNI,fechaNac,parentesco,tlf,email) values ('1',?,?,?,?,?,?,?,?)");			
-			st.setString(1, nombre);
-			st.setString(2, apellido1);
-			st.setString(3, apellido2);
-			st.setString(4, DNI);
-			st.setDate(5,fechaNac);
-			st.setString(6, parentesco);
-			st.setString(7, telefono);
-			st.setString(8, email);
-			st.executeUpdate();
-			return true;
+			
+			/*Validamos si el tutor a insertar ya existe*/		
+			PreparedStatement val = con.prepareStatement("SELECT idTutor FROM tutor WHERE idAlumno = ?");
+			val.setInt(1, idAlumno);		
+			ResultSet rs = val.executeQuery();
+			
+			/*Si no existe en la base de datos lo insertamos en la tabla*/
+			if (!rs.next()){
+				PreparedStatement st = con
+						.prepareStatement("INSERT INTO tutor (idAlumno,nombre,apellido1,apellido2,DNI,fechaNac,parentesco,tlf,email) values ('1',?,?,?,?,?,?,?,?)");			
+				st.setString(1, nombre);
+				st.setString(2, apellido1);
+				st.setString(3, apellido2);
+				st.setString(4, DNI);
+				java.sql.Date fecha = new java.sql.Date (fechaNac.getTime());
+				st.setDate(5,fecha);
+				st.setString(6, parentesco);
+				st.setString(7, telefono);
+				st.setString(8, email);
+				st.executeUpdate();
+				
+				/*Recupero el idTutor que acabamos de crear*/
+				PreparedStatement st2 = con.prepareStatement("SELECT idTutor FROM tutor WHERE DNI LIKE ?");
+				st2.setString(1, DNI);
+				ResultSet rs2 = st2.executeQuery();
+					if (rs.next()){
+						//inserto el tutor en la tabla de relaciones
+						PreparedStatement st3 = con.prepareStatement("INSERT INTO relacionAlumnoTutor(idAlumno, idTutor) values ('1',?) ");
+						st3.setInt(1, rs.getInt("idTutor"));		
+					}
+			}else{
+				/*Si existe solo insertamos la relacion*/
+				PreparedStatement st = con.prepareStatement("INSERT INTO relacionAlumnoTutor(idAlumno, idTutor) values ('1',?) ");
+				st.setInt(1, rs.getInt("idTutor"));		
 			}
-			catch (Exception e) 
-			{
+			
+			return true;
+		}
+		catch (Exception e) 
+		{
 				Logger.getLogger(getClass().getName()).log(Level.SEVERE,
 					"Error en TutorDAO.insertarTutor:"
 							+ e.getMessage());
-			}
-			return false;
+			
+		}
+		return false;
 	}
 
 		
